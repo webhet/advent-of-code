4fb23226691e276e677fb0cf4a87bc7d2f71a617
webhet <marc.hetche@gmail.com>
2024-12-04 19:23:57 +0100

    day4

---
+++ 2024/day4/part1/src/main.rs
@@ -0,0 +1,124 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    let Some(filepath) = env::args().nth(1) else {
+        println!("Usage: part1 <filepath>");
+        return;
+    };
+
+    let file = match File::open(filepath) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Error opening input file: {err}");
+            return;
+        }
+    };
+
+    let reader = BufReader::new(file);
+
+    let grid = reader
+        .lines()
+        .map(|line| {
+            line.expect("Failed to read line")
+                .chars()
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut sum = 0;
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            sum += check_pos(&grid, x as isize, y as isize);
+        }
+    }
+
+    println!("Sum: {sum}")
+}
+
+fn check_pos(grid: &[Vec<char>], x: isize, y: isize) -> usize {
+    let mut pos_sum = 0;
+
+    if get_pos(grid, x, y) != Some('X') {
+        return 0;
+    }
+
+    // top
+    if get_pos(grid, x, y - 1) == Some('M')
+        && get_pos(grid, x, y - 2) == Some('A')
+        && get_pos(grid, x, y - 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // top-right
+    if get_pos(grid, x + 1, y - 1) == Some('M')
+        && get_pos(grid, x + 2, y - 2) == Some('A')
+        && get_pos(grid, x + 3, y - 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // right
+    if get_pos(grid, x + 1, y) == Some('M')
+        && get_pos(grid, x + 2, y) == Some('A')
+        && get_pos(grid, x + 3, y) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // bottom-right
+    if get_pos(grid, x + 1, y + 1) == Some('M')
+        && get_pos(grid, x + 2, y + 2) == Some('A')
+        && get_pos(grid, x + 3, y + 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // bottom
+    if get_pos(grid, x, y + 1) == Some('M')
+        && get_pos(grid, x, y + 2) == Some('A')
+        && get_pos(grid, x, y + 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // bottom-left
+    if get_pos(grid, x - 1, y + 1) == Some('M')
+        && get_pos(grid, x - 2, y + 2) == Some('A')
+        && get_pos(grid, x - 3, y + 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // left
+    if get_pos(grid, x - 1, y) == Some('M')
+        && get_pos(grid, x - 2, y) == Some('A')
+        && get_pos(grid, x - 3, y) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    // top-left
+    if get_pos(grid, x - 1, y - 1) == Some('M')
+        && get_pos(grid, x - 2, y - 2) == Some('A')
+        && get_pos(grid, x - 3, y - 3) == Some('S')
+    {
+        pos_sum += 1;
+    }
+
+    pos_sum
+}
+
+fn get_pos(grid: &[Vec<char>], x: isize, y: isize) -> Option<char> {
+    if x < 0 || y < 0 {
+        None
+    } else {
+        grid.get(y as usize)
+            .and_then(|r| r.get(x as usize).copied())
+    }
+}
